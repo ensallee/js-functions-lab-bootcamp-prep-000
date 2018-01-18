23bf6833f87acc80f699d09746f85973ba008491
Betsy Sallee <ensallee@gmail.com>
2018-01-18 20:41:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // write your code below!
+<<<<<<< HEAD
 function happyHolidays(){
+=======
+function happyHolidays (){
+>>>>>>> 6f71283a05386122e662a1c193bed905fd0ecc7a
   return "Happy holidays!";
 }
 
@@ -8,7 +12,11 @@ function happyHolidaysTo(name){
 }
 
 function happyHolidayTo(holiday, name){
+<<<<<<< HEAD
   return `Happy ${holiday}, ${name}!`
+=======
+  return `Happy ${holiday}, ${name}!`;
+>>>>>>> 6f71283a05386122e662a1c193bed905fd0ecc7a
 }
 
 function holidayCountdown(holiday, days){
